3d26a12a79c446eeb2a9c0c73f55aa7e64a50315
SofiAlfonso <asalfonsom@eafit.edu.co>
2025-02-28 01:35:33 -0500

    Solución del maze con movimientos del robot, finalizada

--- tb3_BFS_Search.py
+++ tb3_BFS_Search.py
@@ -3,6 +3,26 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from collections import deque
 import time
+import heapq
+import math
+
+class Nodo:
+    def __init__(self, position, parent=None,  path_cost=0, action=None,):
+        self.position = position
+        self.parent = parent
+        self.path_cost = path_cost
+        self.action=action
+        
+
+    def __lt__(self, other):
+        return self.path_cost < other.path_cost
+    
+class Problem:
+    def __init__(self, maze, start, end,actions):
+        self.maze = maze
+        self.start = start
+        self.end = end
+        self.actions=actions
 
 class BFSSearch(Node):
     def __init__(self):
@@ -11,16 +31,6 @@ class BFSSearch(Node):
         self.timer = self.create_timer(1.0, self.execute_path)
         
         # Definir secuencia de movimientos basada en BFS
-        path = self.bfs_algorithm()
-        new_path = []
-        # traducir las acciones a comandos coherentes para el robot
-        self.moves = deque(path) # Convierte el path una cola doble
-        self.current_move = None
-        self.start_time = None
-
-    def bfs_algorithm(self):
-        # Método que el estudiante debe completar para calcular el path solución
-        # Retorna un array de comandos de dirección 'forward', 'left', ...
         maze = [
             ["#", "#", "#", "#", "#", "#", "#","#", "#", "#", "#", "#", "#"],
             ["#", " ", " ", " ", " ", " ", " "," ", "#", "#", "#", " ", "E"],
@@ -32,46 +42,131 @@ class BFSSearch(Node):
             ["#", " ", " ", " ", "#", " ", "#"," ", "#", " ", " ", " ", "#"],
             ["#", "#", "#", "#", "#", "#", "#","#", "#", "#", "#", "#", "#"]
         ]
+        path = self.bfs_algorithm(maze)
+        new_path = []
+        # traducir las acciones a comandos coherentes para el robot
+        self.moves = deque(path)
+        self.current_move = None
+        self.start_time = None
+    
+    def reconstruct_path(self, node):
+        instructions=[]
+        actions=[]
+        while node.parent is not None:
+            instructions.append(node.action)
+            node = node.parent
+        
+        instructions.reverse()
+        direction="down"
+        sum_same_directions=0
+        for i in instructions:
+            if direction== i:
+                sum_same_directions+=1
+            else:
+                for a in range(0,math.ceil(sum_same_directions/2)):
+                    actions.append("forward")
+                if direction=="down": 
+                    if i=="left":
+                        actions.append("turn right")
+                        direction="left"
+                        sum_same_directions=1
+                    elif i=="right":
+                        actions.append("turn left")
+                        direction="right"
+                        sum_same_directions=1
+                    elif i=="up":
+                        actions.append("turn right")
+                        actions.append("turn right")
+                        direction="up"
+                        sum_same_directions=1
+            
+                elif direction=="up":
+                    if i=="left":
+                        actions.append("turn left")
+                        direction="left"
+                        sum_same_directions=1
+                    elif i=="right":
+                        actions.append("turn right")
+                        direction="right"
+                        sum_same_directions=1
+                    elif i=="down":
+                        actions.append("turn right")
+                        actions.append("turn right")
+                        direction="down"
+                        sum_same_directions=1
+                
+                elif direction=="left":
+                    if i=="up":
+                        actions.append("turn right")
+                        direction="up"
+                        sum_same_directions=1
+                    elif i=="right":
+                        actions.append("turn right")
+                        actions.append("turn right")
+                        direction="right"
+                        sum_same_directions=1
+                    elif i=="down":
+                        actions.append("turn left")
+                        direction="dowm"
+                        sum_same_directions=1
+                
+                elif direction=="right":
+                    if i=="up":
+                        actions.append("turn left")
+                        direction="up"
+                        sum_same_directions=1
+                    elif i=="left":
+                        actions.append("turn right")
+                        actions.append("turn right")
+                        direction="left"
+                        sum_same_directions=1
+                    elif i=="down":
+                        actions.append("turn right")
+                        direction="down"
+                        sum_same_directions=1
+        
+        return actions
 
-        start = (3,1)
+    def bfs_algorithm(self,maze):
+        #Definición del problema
+        start = (3, 1) 
         end = (1, 12)
-
         actions = {
             "up": (-1, 0),
             "down": (1, 0),
-            "right":(0, 1),
-            "left": (0, -1)
+            "left": (0, -1),
+            "right": (0, 1)
         }
-        queue = deque([start]) # Cola donde vamos a almacenar todos los caminos posibles
-        reached = {start : None}
+        problem=Problem(maze, start, end,actions)
 
-        while queue:
-            x, y = queue.popleft() # Saca la posición primera en la cola
-            print(f"Visitando: {(x, y)}")
-            if (x, y) == end: # Verifica que ya estemos en el final
-                print("¡Meta alcanzada!")
-                return self.reconstruct_path(start, end, reached)
+        def manhatan_distance(pos, goal):
+            return abs(pos[0] - goal[0]) + abs(pos[1] - goal[1])  # Distancia de Manhattan
+
+        def get_neighbors(pos):
+            neighbors = [] #lista de vecinos
+            for action, (dx, dy) in problem.actions.items():  #Tenga en cuenta que para que esto sea funcional ya debio haber definido el objeto problem
+                neighbor = (pos[0] + dx, pos[1] + dy)
+                if maze[neighbor[0]][neighbor[1]] != "#": #si el vecino es diferente a "#" pared agregarlo a la lista de vecinos                neighbors.append(neighbor)
+                    neighbors.append((neighbor,action))
+            return neighbors
+
+        start_node = Nodo(start, path_cost=0)
+        frontier = [(manhatan_distance(start, end), start_node)]
+        heapq.heapify(frontier) #Convierte la lista frontier en una cola de prioridad (heap)
+        reached = {start: start_node}
+
+        while frontier:
+            _, node = heapq.heappop(frontier)
+            if node.position == end:
+                return self.reconstruct_path(node)
             
-            for direction, (dx, dy) in actions.items():
-                nx, ny = (x + dx, y + dy)
-
-                if 0 <= nx < len(maze) and 0 <= ny < len(maze[0]) and maze[nx][ny] in [" ", "E"] and (nx, ny) not in reached:
-                    queue.append((nx, ny))
-                    reached[(nx, ny)] = (x, y, direction)
-        return [] # No encontro camino/salida
-        
-    
-    def reconstruct_path(self, start, end, reached):
-        path = []
-        current = end # Partimos cuando ya hemos llegado al final, y empezamos a reconstruir el camino hacia atrás
-
-        while current != start: # Sí el actual es igual al inicio, ya reconstruimos todo el camino
-            prev, action = reached[current][0:2], reached[current][2]  # Toma el x, y, y ya en action la dirección
-            path.append(action)
-            current = prev
-        path.reverse()
-        print("Camino reconstruido:", path)
-        return path
+            for neighbor, action in get_neighbors(node.position):
+                new_cost = node.path_cost + 1
+                if neighbor not in reached or new_cost < reached[neighbor].path_cost:
+                    reached[neighbor] = Nodo(neighbor, parent=node,action=action, path_cost=new_cost)
+                    heapq.heappush(frontier, (manhatan_distance(neighbor, end), reached[neighbor]))
+                    
+        return None
 
     def execute_path(self):
         twist = Twist()
@@ -91,23 +186,23 @@ class BFSSearch(Node):
         
         action = self.current_move
         
-        if action == 'up' or action == 'down':
+        if action == 'forward':
             duration = 5.0
-        elif action == 'left':
-            duration = 3.15
-        elif action == 'right':
-            duration = 3.15
+        elif action == 'turn left':
+            duration = 3.62
+        elif action == 'turn right':
+            duration = 3.62
         else:
             duration = 0
 
         
-        if action == 'up' or action == 'down':
-            twist.linear.x = 0.2  # Movimiento hacia adelante
+        if action == 'forward':
+            twist.linear.x = 0.24 # Movimiento hacia adelante
             twist.angular.z = 0.0
-        elif action == 'left':
+        elif action == 'turn left':
             twist.linear.x = 0.0
             twist.angular.z = 0.5  # Gira a la izquierda
-        elif action == 'right':
+        elif action == 'turn right':
             twist.linear.x = 0.0
             twist.angular.z = -0.5  # Gira a la derecha
         
@@ -116,8 +211,7 @@ class BFSSearch(Node):
         while time.time() - self.start_time < duration:
             pass
 
-        self.current_move = None  # Pasar al siguiente movimiento
-
+        self.current_move = None
 
 def main(args=None):
     rclpy.init(args=args)
